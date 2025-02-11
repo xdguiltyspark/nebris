c7e87fc727cfe51a2c79ea6b4ab9f096983b3189
xdguiltyspark <144620033+xdguiltyspark@users.noreply.github.com>
2025-02-11 12:52:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Download URL
-theme_url="https://raw.githubusercontent.com/xdguiltyspark/Nebris/main"
+theme_url="https://raw.githubusercontent.com/xdguiltyspark/nebris/main"
 
 # Setup directories to download to
 spice_dir="$(dirname "$(spicetify -c)")"
@@ -14,9 +14,9 @@ mkdir -p "${theme_dir}/Nebris"
 
 # Download latest tagged files into correct director
 echo "Downloading Nebris..."
-curl --silent --output "${theme_dir}/Nebris/color.ini" "${theme_url}/color.ini"
-curl --silent --output "${theme_dir}/Nebris/user.css" "${theme_url}/user.css"
-curl --silent --output "${theme_dir}/Nebris/theme.js" "${theme_url}/theme.js"
+curl --silent --output "${theme_dir}/nebris/color.ini" "${theme_url}/color.ini"
+curl --silent --output "${theme_dir}/nebris/user.css" "${theme_url}/user.css"
+curl --silent --output "${theme_dir}/nebris/theme.js" "${theme_url}/theme.js"
 echo "Done"
 
 # Apply theme
